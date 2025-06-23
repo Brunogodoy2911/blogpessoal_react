f90f1a5e3223f62c7427e8e0cd07d74f6aabc4aa
Brunogodoy2911 <brunogodoy29@hotmail.com>
2025-06-23 15:11:02 -0300

    :sparkles: Consumindo a API para realizar o cadastro

--- src/pages/cadastro/Cadastro.tsx
+++ src/pages/cadastro/Cadastro.tsx
@@ -1,9 +1,185 @@
-import React from 'react'
+import React from "react";
+import "./cadastro.css";
 
-export default function Cadastro() {
+import type Usuario from "../../models/Usuario";
+
+import { useNavigate } from "react-router";
+import { cadastrarUsuario } from "../../services/Service";
+import { RotatingLines } from "react-loader-spinner";
+
+function Cadastro() {
+  const navigate = useNavigate();
+
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [confirmarSenha, setConfirmaSenha] = React.useState("");
+
+  const [usuario, setUsuario] = React.useState<Usuario>({
+    id: 0,
+    nome: "",
+    usuario: "",
+    senha: "",
+    foto: "",
+  });
+
+  React.useEffect(() => {
+    if (usuario.id !== 0) {
+      retornar();
+    }
+  }, [usuario]);
+
+  function retornar() {
+    navigate("/");
+  }
+
+  function atualizarEstado(e: React.ChangeEvent<HTMLInputElement>) {
+    setUsuario({
+      ...usuario,
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  function handleConfirmarSenha(e: React.ChangeEvent<HTMLInputElement>) {
+    setConfirmaSenha(e.target.value);
+  }
+
+  async function cadastrarNovoUsuario(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    if (confirmarSenha === usuario.senha && usuario.senha.length >= 8) {
+      try {
+        setIsLoading(true);
+        await cadastrarUsuario("/usuarios/cadastrar", usuario, setUsuario);
+        alert("Usuário cadastrado com sucesso!");
+      } catch (e) {
+        alert("Erro ao cadastrar o usuário");
+        console.log(e);
+      } finally {
+        setIsLoading(false);
+      }
+    } else {
+      alert(
+        "Dados do usuário inconsistentes! Verifique as informações do cadastro"
+      );
+      setUsuario({
+        ...usuario,
+        senha: "",
+      });
+    }
+  }
+
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <div
+        className="grid grid-cols-1 lg:grid-cols-2 h-screen 
+            place-items-center font-bold"
+      >
+        <div className="fundoCadastro hidden lg:block"></div>
+        <form
+          onSubmit={cadastrarNovoUsuario}
+          className="flex justify-center items-center flex-col w-2/3 gap-3"
+        >
+          <h2 className="text-slate-900 text-5xl">Cadastrar</h2>
+          <div className="flex flex-col w-full">
+            <label htmlFor="nome">Nome</label>
+            <input
+              type="text"
+              id="nome"
+              name="nome"
+              placeholder="Nome"
+              className="border-2 border-slate-700 rounded p-2"
+              value={usuario.nome}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                atualizarEstado(e)
+              }
+            />
+          </div>
+          <div className="flex flex-col w-full">
+            <label htmlFor="usuario">Usuario</label>
+            <input
+              type="text"
+              id="usuario"
+              name="usuario"
+              placeholder="Usuario"
+              className="border-2 border-slate-700 rounded p-2"
+              value={usuario.usuario}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                atualizarEstado(e)
+              }
+            />
+          </div>
+          <div className="flex flex-col w-full">
+            <label htmlFor="foto">Foto</label>
+            <input
+              type="text"
+              id="foto"
+              name="foto"
+              placeholder="Foto"
+              className="border-2 border-slate-700 rounded p-2"
+              value={usuario.foto}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                atualizarEstado(e)
+              }
+            />
+          </div>
+          <div className="flex flex-col w-full">
+            <label htmlFor="senha">Senha</label>
+            <input
+              type="password"
+              id="senha"
+              name="senha"
+              placeholder="Senha"
+              className="border-2 border-slate-700 rounded p-2"
+              value={usuario.senha}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                atualizarEstado(e)
+              }
+            />
+          </div>
+          <div className="flex flex-col w-full">
+            <label htmlFor="confirmarSenha">Confirmar Senha</label>
+            <input
+              type="password"
+              id="confirmarSenha"
+              name="confirmarSenha"
+              placeholder="Confirmar Senha"
+              className="border-2 border-slate-700 rounded p-2"
+              value={confirmarSenha}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                handleConfirmarSenha(e)
+              }
+            />
+          </div>
+          <div className="flex justify-around w-full gap-8">
+            <button
+              type="reset"
+              className="rounded text-white bg-red-400 hover:bg-red-700 w-1/2 py-2"
+              onClick={retornar}
+            >
+              Cancelar
+            </button>
+            <button
+              type="submit"
+              className="rounded text-white bg-indigo-400 
+                           hover:bg-indigo-900 w-1/2 py-2
+                           flex justify-center"
+            >
+              {isLoading ? (
+                <RotatingLines
+                  strokeColor="white"
+                  strokeWidth="5"
+                  animationDuration="0.75"
+                  width="24"
+                  visible={true}
+                />
+              ) : (
+                <span>Cadastrar</span>
+              )}
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
 }
+
+export default Cadastro;
